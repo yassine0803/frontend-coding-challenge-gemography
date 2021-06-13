6650632def7ea0bbf0e09a2d5cce437435543990
yassine aourhebal <aourhebal.yassine@gmail.com>
2021-06-13 01:50:12 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {useState} from 'react';
-import Navbar from "./components/Navbar/Navbar";
+import {Navbar, Repositories} from "./components";
 import useFetchRepositories from './useFetchRepositories';
-import Repositories from "./components/Repositories/Repositories";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(0);
